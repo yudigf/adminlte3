967ad15169f37a75ec7d84a99af1713a3a8c51e6
yudigf08 <yudigf08@gmail.com>
2023-06-16 15:12:10 +0700

    menu side bar belum diubah

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,8 @@
+<!-- /.content-wrapper -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023 PT. JAYADISA</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Yorozuya Version App</b> 1.0.0
+    </div>
+  </footer>
